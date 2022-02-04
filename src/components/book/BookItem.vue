30894b592d9ac46bf5d8eb0faac3e7e381a304c4
flaviosalgado <flavio@canaltelecom.com.br>
2022-02-03 22:27:44 -0300

    imagens e links em cards dos livros

--- src/components/book/BookItem.vue
+++ src/components/book/BookItem.vue
@@ -1,32 +1,47 @@
 <template>   
     <v-card class="mx-auto book-card">
-    <v-card-title>
-        <span>{{ book.volumeInfo.title.substring(0, maxTitleLenght) }}</span>
-        <span v-if="book.volumeInfo.title.lenght > maxTitleLenght">...</span>
-    </v-card-title>
+        <div v-if="book.volumeInfo.imageLinks && book.volume.imageLinks.smallThumbnail" class="text-center pt-3">
+            <img :src="book.volumeInfo.imageLinks.smallThumbnail">
+        </div>
+        <v-card-title>
+            <span>{{ book.volumeInfo.title.substring(0, maxTitleLenght) }}</span>
+            <span v-if="book.volumeInfo.title.lenght > maxTitleLenght">...</span>
+        </v-card-title>
 
-    <v-card-text>
-        {{ book.volumeInfo.subtitle || 'Sem descrição' }}
-    </v-card-text>
+        <v-card-text>
+            {{ book.volumeInfo.subtitle || 'Sem descrição' }}
+        </v-card-text>
+        
+        <v-card-actions>
+            <v-btn text small color="primary" @click="goToDetails">Ver Detalhes</v-btn>
+            <v-btn v-if="book.volumeInfo.previewLink" text small color="primary" @click="goToPreview">Ver Preview</v-btn>
+        </v-card-actions>
     
-    <v-card-actions>
-        <v-btn text small color="primary">Ver Detalhes</v-btn>
-    </v-card-actions>
     </v-card>           
 </template>
 
 <script>
 
-    export default {
-        
+    export default {        
         name: 'BookList',
         props: {
             book: { type: Object, required: true },
         },
+        created(){
+            console.log(this.book);
+        },
         data(){
             return{
-                maxTitleLenght: 20
+                maxTitleLenght: 20,
             };
+        },
+        methods: {
+            goToDetails() {
+
+            },
+            goToPreview(){
+                window.open(this.book.volumeInfo.previewLink, '_blank');
+            }
         }
         
     }
